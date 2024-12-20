3b6b14bd384a11eac867ef5993e66c4279ee34fd
Li Zhao <lz1181@gadi-cpu-bdw-0018.gadi.nci.org.au>
2024-12-20 16:49:53 +1100

    path updated

--- gadi_tasks/au_recurrent_qsubs/au_fmc_mosaics_qsub_rec.py
+++ gadi_tasks/au_recurrent_qsubs/au_fmc_mosaics_qsub_rec.py
@@ -1,6 +1,6 @@
 import os
 
 if __name__ == "__main__":
-    for year in range(2024,2025):
+    for year in range(2025,2026):
         print(year)
-        os.system('qsub -v "year={0}" /g/data/xc0/user/scortechini/github/fire-data-processing/gadi_tasks/au_recurrent_qsubs/au_fmc_mosaics_rec.qsub'.format(year))
+        os.system('qsub -v "year={0}" /g/data/wj98/AFMS/fire-data-processing/gadi_tasks/au_recurrent_qsubs/au_fmc_mosaics_rec.qsub'.format(year))
--- gadi_tasks/au_recurrent_qsubs/rank_with_deciles_rec.py
+++ gadi_tasks/au_recurrent_qsubs/rank_with_deciles_rec.py
@@ -2,4 +2,4 @@ import os
 
 if __name__ == "__main__":
     for var in ['fmc','flam']:
-        os.system('qsub -v "var={0}" /g/data/xc0/user/scortechini/github/fire-data-processing/gadi_tasks/au_recurrent_qsubs/rank_with_deciles_rec.qsub'.format(var))
+        os.system('qsub -v "var={0}" /g/data/wj98/AFMS/fire-data-processing/gadi_tasks/au_recurrent_qsubs/rank_with_deciles_rec.qsub'.format(var))
